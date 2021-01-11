81c24aa1f5f5f2fa11a193f45a34b0d256529cc7
Rafael Ortiz <rafaelortiz99@outlook.com>
2021-01-11 11:58:24 -0500

    user register

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 function home(req, res){
@@ -14,7 +15,36 @@ function pruebas(req, res){
   });
 }
 
+function saveUser(req, res){
+  var user = new User();
+  var params = req.body;
+
+  if(params.name && params.lastname && params.nick && params.email && params.password){
+    user.name = params.name;
+    user.lastname = params.lastname;
+    user.nick = params.nick;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.img = null;
+
+    bcrypt.hash(params.password, null, null, (err, hash) =>{
+        user.password = hash;
+        user.save((err, userStore) => {
+          if(err) return res.status(500).send('Error at save user');
+          userStore ? res.status(200).send({user: userStore}) : res.status(404).send({message: 'Error at register'});
+        });
+    });
+  }
+  else{
+    res.status(500).send({
+      message: 'Incomplete Data'
+    });
+  }
+
+}
+
 module.exports = {
   home,
-  pruebas
+  pruebas,
+  saveUser
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -6,5 +6,6 @@ var api = express.Router();
 
 api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.post('/register', UserController.saveUser);
 
 module.exports = api;
