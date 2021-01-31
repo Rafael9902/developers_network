d2d98dcd9365c4c52a33aee62ad2705a2bf1d2c3
Rafael9902 <rafaelortiz99@outlook.com>
2021-01-31 15:44:35 -0500

    Formulario de registro

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingProviders, routing } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [AppRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
