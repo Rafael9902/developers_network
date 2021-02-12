bf2521060d101f23c06542c128dffbe9f8e8d239
Rafael9902 <rafaelortiz99@outlook.com>
2021-02-12 12:26:51 -0500

    update users

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,15 +2,17 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //Componentes
-import { LoginComponent } from './components/login/login.component'
-import { RegisterComponent } from './components/register/register.component'
-import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 const app_routes: Routes = [
     {path: '', component:HomeComponent},
     {path: 'home', component:HomeComponent},
     {path: 'login', component:LoginComponent},
-    {path: 'register', component:RegisterComponent}
+    {path: 'register', component:RegisterComponent},
+    {path: 'my-data', component:UserEditComponent}
 ];
 
 export const AppRoutingProviders: any[] = [];
